b62c6d3a57382366fdd57b89bc6e1aa490542337
Randy Reddig <ydnar@shaderlab.com>
2018-07-14 10:41:49 -0700

    Rename data directory to testdata, per:
    https://github.com/golang/go/commit/2f9eeea212681927385a4713004438cd864966e0

--- whoistest.go
+++ whoistest.go
@@ -13,11 +13,11 @@ var (
 // ResponseFiles returns a slice of paths to MIME-encoded whois responses.
 // Returns nil, error if any errors occur.
 func ResponseFiles() ([]string, error) {
-	return filepath.Glob(filepath.Join(_dir, "data", "responses", "*", "*.mime"))
+	return filepath.Glob(filepath.Join(_dir, "testdata", "responses", "*", "*.mime"))
 }
 
 // ResponseFilename returns a fully-qualified path to a response file
 // for the given query and host.
 func ResponseFilename(query, host string) string {
-	return filepath.Join(_dir, "data", "responses", host, query+".mime")
+	return filepath.Join(_dir, "testdata", "responses", host, query+".mime")
 }
